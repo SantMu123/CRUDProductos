854822e99de350a1eeb7a8ef648f5ab89be5b252
SantMu123 <smunozcastaneda14@gmail.com>
2024-11-08 18:21:28 -0500

    feat: :sparkles: conexion con endpoints php en frontend

---
+++ frontend/services/app.service.js
@@ -0,0 +1,27 @@
+angular.module('productApp').service('productService', function($http) {
+    const apiUrl = 'http://localhost:3600';
+
+    this.getAllProducts = function() {
+        return $http.get(`${apiUrl}/products`);
+    };
+
+    this.getProductsByCategory = function(categoryId) {
+        return $http.get(`${apiUrl}/products/category/${categoryId}`);
+    };
+
+    this.getAllCategories = function() {
+        return $http.get(`${apiUrl}/categories`);
+    };
+
+    this.createProduct = function(product) {
+        return $http.post(`${apiUrl}/create`, product);
+    };
+
+    this.updateProduct = function(product) {
+        return $http.put(`${apiUrl}/products/${product.id}`, product);
+    };
+
+    this.deleteProduct = function(id) {
+        return $http.delete(`${apiUrl}/products/${id}`);
+    };
+});
