291314703e8a7f5ddb35a783f7ffd2ad9672f1fc
SantMu123 <smunozcastaneda14@gmail.com>
2024-11-09 01:42:46 -0500

    feat: :fire: remover código innecesario

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -1,14 +1,8 @@
 angular.module('productApp')
     .config(['$routeProvider', function config($routeProvider) {
         $routeProvider
-            // Ruta principal donde se carga la lista de productos y categorías
             .when('/', {
                 template: '<product-list></product-list>'
-            })
-            // Ruta para cargar productos por categoría
-            .when('/category/:categoryId', {
-                template: '<product-list></product-list>'
-
             })
             .otherwise('/');
     }]);
